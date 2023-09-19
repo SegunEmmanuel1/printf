6e30d90a3d8e4fac027e6c72a7e74972942aba7e
SegunEmmanuel1 <segunemmanuel67@yahoo.com>
2023-09-19 15:49:46 -0700

    updated

--- main.h
+++ main.h
@@ -1,50 +1,46 @@
-#ifndef MAIN_H
-#define MAIN_H
 
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdarg.h>
+/* width handler */
 
-/* utils.c */
-int _strlen(const char *);
-int print(char *);
-char *itoa(long int, int);
+int handle_write_char(char c, char buffer[],
 
-/* printf.c */
-int _printf(const char *, ...);
+int flags, int width, int precision, int size);
 
-/* handler.c */
-int handler(const char *, va_list);
-int percent_handler(const char *, va_list, int *);
+int write_number(int is_positive, int ind, char buffer[],
 
-/* printers */
-int print_string(va_list);
-int print_char(va_list);
-int print_integer(va_list);
-int print_binary(va_list);
-int print_rot(va_list);
-int print_unsigned(va_list);
-int print_octal(va_list);
-int print_hexadecimal_low(va_list);
-int print_hexadecimal_upp(va_list);
-int print_pointer(va_list);
-int print_rev_string(va_list);
+int flags, int width, int precision, int size);
 
-/* _putchar.c */
-int _putchar(char);
-int buffer(char);
+int write_num(int ind, char bff[], int flags, int width, int precision,
 
-/**
- * struct _format - Typedef struct
- *
- * @type: Format
- * @f: The function associated
- **/
-typedef struct _format
-{
-	char type;
-	int (*f)(va_list);
-} format;
+int length, char padd, char extra_c);
+
+int write_pointer(char buffer[], int ind, int length,
+
+int width, int flags, char padd, char extra_c, int padd_start);
+
+
+
+int write_unsgnd(int is_negative, int ind,
+
+char buffer[],
+
+int flags, int width, int precision, int size);
+
+
+
+/****************** UTILS ******************/
+
+int is_printable(char);
+
+int append_hexa_code(char, char[], int);
+
+int is_digit(char);
+
+
+
+long int convert_size_number(long int num, int size);
+
+long int convert_size_unsgnd(unsigned long int num, int size);
+
 
 
 #endif /* MAIN_H */
